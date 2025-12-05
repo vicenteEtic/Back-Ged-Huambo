d389e30552d5498feebc05aaa2f5ce4e0f4afcde
root <vicentemanueleduardo@gmail.com>
2025-12-05 10:11:32 +0100

    fix:generate alert

--- app/External/PepExternalApi.php
+++ app/External/PepExternalApi.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Http;
 
 class PepExternalApi
 {
+    /**
+     * Buscar dados de PEP por nome
+     */
     public static function getDataPepExternal($name)
     {
         $baseUrl = config('services.pep.url');
 
         // Timeout de 60 segundos, retry 3 vezes e ignorar verificação SSL
         $api = Http::timeout(60)
-            ->retry(3, 1000)
+            ->retry(5, 1000)
             ->withOptions(['verify' => false])
             ->get("{$baseUrl}/pep/search", [
                 "filters" => [
@@ -32,6 +35,9 @@ class PepExternalApi
         ], $api->status());
     }
 
+    /**
+     * Buscar todos os dados de PEP ou filtrados por nome
+     */
     public static function getAllPep($name = null)
     {
         $baseUrl = config('services.pep.url');
