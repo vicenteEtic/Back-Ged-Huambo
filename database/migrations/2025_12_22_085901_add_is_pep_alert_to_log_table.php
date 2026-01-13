25a96867141ed11e392963ca4fc585ccfa940ecd
root <vicentemanueleduardo@gmail.com>
2026-01-13 12:54:42 +0100

    fix:alert generate

--- database/migrations/2025_12_22_085901_add_is_pep_alert_to_log_table.php
+++ database/migrations/2025_12_22_085901_add_is_pep_alert_to_log_table.php
@@ -5,47 +5,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
+   public function up(): void
     {
-        $columnsToDrop = [];
+        Schema::table('alert', function (Blueprint $table) {
+            $table->boolean('is_pep')->nullable()
+                ->comment('Indica se a entidade é Politically Exposed Person');
 
-        if (Schema::hasColumn('alert', 'is_pep')) {
-            $columnsToDrop[] = 'is_pep';
-        }
+            $table->boolean('is_sanctioned')->nullable()
+                ->comment('Indica se a entidade consta em listas de sanções');
 
-        if (Schema::hasColumn('alert', 'is_sanctioned')) {
-            $columnsToDrop[] = 'is_sanctioned';
-        }
-
-        if (Schema::hasColumn('alert', 'is_reported')) {
-            $columnsToDrop[] = 'is_reported';
-        }
-
-        if (!empty($columnsToDrop)) {
-            Schema::table('alert', function (Blueprint $table) use ($columnsToDrop) {
-                $table->dropColumn($columnsToDrop);
-            });
-        }
+            $table->boolean('is_reported')->nullable()
+                ->comment('Detalhes de reporte: comunicação, data, entidade reguladora');
+        });
     }
 
     public function down(): void
     {
         Schema::table('alert', function (Blueprint $table) {
-
-            if (!Schema::hasColumn('alert', 'is_pep')) {
-                $table->boolean('is_pep')->nullable()
-                    ->comment('Indica se a entidade é Politically Exposed Person');
-            }
-
-            if (!Schema::hasColumn('alert', 'is_sanctioned')) {
-                $table->boolean('is_sanctioned')->nullable()
-                    ->comment('Indica se a entidade consta em listas de sanções');
-            }
-
-            if (!Schema::hasColumn('alert', 'is_reported')) {
-                $table->boolean('is_reported')->nullable()
-                    ->comment('Detalhes de reporte: comunicação, data, entidade reguladora');
-            }
+            $table->dropColumn([
+                'is_pep',
+                'is_sanctioned',
+                'is_reported',
+            ]);
         });
     }
 };
