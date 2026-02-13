45537b32c518c4969f43826dc76f294efa56230f
root <vicentemanueleduardo@gmail.com>
2026-02-13 11:27:58 +0100

    fix:alert

--- app/Repositories/Alert/AlertRepository.php
+++ app/Repositories/Alert/AlertRepository.php
@@ -167,7 +167,6 @@ class AlertRepository extends AbstractRepository
             'by_month' => $this->getTotalAlertsByMonth(),
         ];
     }
-
     /**
      * Contagem genérica por campo
      */
