a11894ad0ec99b5e6346e206d7ae5aa785a216b6
root <vicentemanueleduardo@gmail.com>
2026-03-09 14:46:39 +0100

    2

--- app/Repositories/Alert/AlertUser/AlertUserRepository.php
+++ app/Repositories/Alert/AlertUser/AlertUserRepository.php
@@ -4,7 +4,6 @@ namespace App\Repositories\Alert\AlertUser;
 
 use App\Models\Alert\AlertUser\AlertUser;
 use App\Repositories\AbstractRepository;
-use App\Repositories\Alert\AlertRepository;
 use App\Services\Log\LogService;
 use App\Services\User\UserService;
 use Illuminate\Http\JsonResponse;
@@ -20,35 +19,33 @@ class AlertUserRepository extends AbstractRepository
     public const STATUS_NEW = 1;
     public const STATUS_VALIDATION = 2;
     public const STATUS_SUPERVISION = 3;
-       private $alert;
 
-    public function __construct(AlertUser $model, UserService $user, LogService $logService,AlertRepository $alert)
+    public function __construct(AlertUser $model, UserService $user, LogService $logService)
     {
 
         $this->user = $user;
         $this->logService = $logService;
-        $this->alert=$alert;
         parent::__construct($model);
     }
 
 
     public function countAlertUser(int $userId, int $status): int
     {
-        return $this->model
-            ->where('user_id', $userId)
-            ->where('is_read', $status)
-            ->count();
+         return $this->model
+        ->where('user_id', $userId)
+        ->where('is_read', $status)
+        ->count();
     }
 
-
+  
     public function countAlertsByUserGrouped(int $userId): array
     {
         return [
-            'total_active' => $this->countAlertUser($userId, self::STATUS_NEW),
-            'closed'       =>  $this->countAlertUser($userId, self::STATUS_CLOSED),
-            'new'          =>  $this->countAlertUser($userId, self::STATUS_NEW),
-            'validation'   =>  $this->countAlertUser($userId, self::STATUS_VALIDATION),
-            'supervision'  =>  $this->countAlertUser($userId, self::STATUS_SUPERVISION),
+            'total_active' => $this->countAlertUser($userId,self::STATUS_NEW),
+            'closed'       =>  $this->countAlertUser($userId,self::STATUS_CLOSED),
+            'new'          =>  $this->countAlertUser($userId,self::STATUS_NEW),
+            'validation'   =>  $this->countAlertUser($userId,self::STATUS_VALIDATION),
+            'supervision'  =>  $this->countAlertUser($userId,self::STATUS_SUPERVISION),
         ];
     }
 
@@ -93,9 +90,7 @@ class AlertUserRepository extends AbstractRepository
         // insere na pivot alert_user
         $inserted = $this->model->insert(
             collect($data)->map(function ($item) use ($now) {
-             $alertData = $this->alert->findByValidate($item['alert_id']);
                 return array_merge($item, [
-                    'is_read'=>  $alertData->is_active,
                     'created_at' => $now,
                     'updated_at' => $now,
                 ]);
