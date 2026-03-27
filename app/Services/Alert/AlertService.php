e57c11be33187c60cb7d311fc8032f4bb12264bd
vicenteeduardo <vicentemanueleduardo@gmail.com>
2026-03-27 17:28:13 +0100

    2

--- app/Services/Alert/AlertService.php
+++ app/Services/Alert/AlertService.php
@@ -22,14 +22,7 @@ class AlertService extends AbstractService
     }
 
 
-    public function index(?int $paginate, ?array $filterParams, ?array $orderByParams, $relationships = [])
-    {
-        $relationships =  [
-            'entity:id,social_denomination,customer_number,policy_number'
-        ];
-        $orderByParams = $orderByParams ?? ['created_at' => 'desc'];
-        return $this->repository->index($paginate, $filterParams, $orderByParams, $relationships);
-    }
+  
     public function getTotalAlerts(): array
     {
         return $this->repository->getTotalAlerts();
