b88208214cf2458dd7ae8d2e6e60347ca1dbbb71
vicenteeduardo <vicentemanueleduardo@gmail.com>
2026-06-03 16:54:53 +0100

    fix: chunkById + tries=10 no ProcessCustomerDataJob para evitar timeouts e MaxAttemptsExceeded

--- app/Jobs/ProcessCustomerDataJob.php
+++ app/Jobs/ProcessCustomerDataJob.php
@@ -16,7 +16,7 @@ class ProcessCustomerDataJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $timeout = 600;
-    public $tries = 3;
+    public $tries = 10;
 
     protected string $numeroCliente;
 
@@ -37,12 +37,11 @@ class ProcessCustomerDataJob implements ShouldQueue
         }
 
         /**
-         * 🔥 Buscar apólices em chunks
+         * 🔥 Buscar apólices com chunkById (evita OFFSET lento)
          */
         DB::table('policies_staging')
             ->where('numero_cliente', $this->numeroCliente)
-            ->orderBy('id')
-            ->chunk(500, function ($policies) use ($entity) {
+            ->chunkById(500, function ($policies) use ($entity) {
 
                 $policyNumbers = collect($policies)
                     ->pluck('numero_apolice')
