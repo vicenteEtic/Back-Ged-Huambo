28205ccc8d49a5bf7de91972724c83cef81955c9
vicenteeduardo <vicentemanueleduardo@gmail.com>
2026-07-17 12:07:40 +0100

    fix: corrigir rota show employee documents

--- routes/rh/employee_document.php
+++ routes/rh/employee_document.php
@@ -7,4 +7,4 @@ Route::get('/', [EmployeeDocumentController::class, 'index'])->name('employee_do
 Route::post('/', [EmployeeDocumentController::class, 'store'])->name('employee_document.store')->middleware(['can:rh-documentos-create']);
 Route::put('{id}', [EmployeeDocumentController::class, 'update'])->name('employee_document.update')->middleware(['can:rh-documentos-edit']);
 Route::delete('{id}', [EmployeeDocumentController::class, 'destroy'])->name('employee_document.destroy')->middleware(['can:rh-documentos-delete']);
-Route::get('{employee_id}', [EmployeeDocumentController::class, 'show'])->name('employee_document.show')->middleware(['can:rh-documentos-show']);
+Route::get('/{employee_id}', [EmployeeDocumentController::class, 'show'])->name('employee_document.show')->middleware(['can:rh-documentos-show']);
