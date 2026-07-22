001a2179a83659c97aae2dc16be3cfd559f39150
vicenteeduardo <vicentemanueleduardo@gmail.com>
2026-07-22 15:13:13 +0100

    fix: corrigir permissoes IRT brackets para rh-processamento-*

--- routes/rh/irt_bracket.php
+++ routes/rh/irt_bracket.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\RH\Payroll\IrtBracketController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IrtBracketController::class, 'index'])->name('irt_bracket.index')->middleware(['can:rh-folha-pagamento-show']);
-Route::post('/', [IrtBracketController::class, 'store'])->name('irt_bracket.store')->middleware(['can:rh-folha-pagamento-create']);
-Route::get('{id}', [IrtBracketController::class, 'show'])->name('irt_bracket.show')->middleware(['can:rh-folha-pagamento-show']);
-Route::put('{id}', [IrtBracketController::class, 'update'])->name('irt_bracket.update')->middleware(['can:rh-folha-pagamento-edit']);
-Route::delete('{id}', [IrtBracketController::class, 'destroy'])->name('irt_bracket.destroy')->middleware(['can:rh-folha-pagamento-delete']);
+Route::get('/', [IrtBracketController::class, 'index'])->name('irt_bracket.index')->middleware(['can:rh-processamento-show']);
+Route::post('/', [IrtBracketController::class, 'store'])->name('irt_bracket.store')->middleware(['can:rh-processamento-create']);
+Route::get('{id}', [IrtBracketController::class, 'show'])->name('irt_bracket.show')->middleware(['can:rh-processamento-show']);
+Route::put('{id}', [IrtBracketController::class, 'update'])->name('irt_bracket.update')->middleware(['can:rh-processamento-edit']);
+Route::delete('{id}', [IrtBracketController::class, 'destroy'])->name('irt_bracket.destroy')->middleware(['can:rh-processamento-delete']);
